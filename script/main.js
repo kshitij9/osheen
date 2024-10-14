5b806d2cfaa4aa664c6dcb364dd4f5f20e63b259
hardeep-96 <kshitijtwr@gmail.com>
2024-10-14 17:09:25 +0530

    test

--- script/main.js
+++ script/main.js
@@ -58,156 +58,156 @@ const animationTimeline = () => {
     .to(".container", 0.1, {
       visibility: "visible"
     })
-    .from(".one", 0.7, {
-      opacity: 0,
-      y: 10
-    })
-    .from(".two", 0.4, {
-      opacity: 0,
-      y: 10
-    })
-    .to(
-      ".one",
-      0.7,
-      {
-        opacity: 0,
-        y: 10
-      },
-      "+=2.5"
-    )
-    .to(
-      ".two",
-      0.7,
-      {
-        opacity: 0,
-        y: 10
-      },
-      "-=1"
-    )
-    .from(".three", 0.7, {
-      opacity: 0,
-      y: 10
-      // scale: 0.7
-    })
-    .to(
-      ".three",
-      0.7,
-      {
-        opacity: 0,
-        y: 10
-      },
-      "+=2"
-    )
-    .from(".four", 0.7, {
-      scale: 0.2,
-      opacity: 0
-    })
-    .from(".fake-btn", 0.3, {
-      scale: 0.2,
-      opacity: 0
-    })
-    .staggerTo(
-      ".hbd-chatbox span",
-      0.5,
-      {
-        visibility: "visible"
-      },
-      0.05
-    )
-    .to(".fake-btn", 0.1, {
-      backgroundColor: "rgb(127, 206, 248)"
-    })
-    .to(
-      ".four",
-      0.5,
-      {
-        scale: 0.2,
-        opacity: 0,
-        y: -150
-      },
-      "+=0.7"
-    )
-    .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
-      scale: 1.2,
-      x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
-    })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(
-      ".idea-5",
-      0.7,
-      {
-        rotationX: 15,
-        rotationZ: -10,
-        skewY: "-5deg",
-        y: 50,
-        z: 10,
-        opacity: 0
-      },
-      "+=0.5"
-    )
-    .to(
-      ".idea-5 .smiley",
-      0.7,
-      {
-        rotation: 90,
-        x: 8
-      },
-      "+=0.4"
-    )
-    .to(
-      ".idea-5",
-      0.7,
-      {
-        scale: 0.2,
-        opacity: 0
-      },
-      "+=2"
-    )
-    .staggerFrom(
-      ".idea-6 span",
-      0.8,
-      {
-        scale: 3,
-        opacity: 0,
-        rotation: 15,
-        ease: Expo.easeOut
-      },
-      0.2
-    )
-    .staggerTo(
-      ".idea-6 span",
-      0.8,
-      {
-        scale: 3,
-        opacity: 0,
-        rotation: -15,
-        ease: Expo.easeOut
-      },
-      0.2,
-      "+=1"
-    )
-    .staggerFromTo(
-      ".baloons img",
-      2.5,
-      {
-        opacity: 0.9,
-        y: 1400
-      },
-      {
-        opacity: 1,
-        y: -1000
-      },
-      0.2
-    )
+    // .from(".one", 0.7, {
+    //   opacity: 0,
+    //   y: 10
+    // })
+    // .from(".two", 0.4, {
+    //   opacity: 0,
+    //   y: 10
+    // })
+    // .to(
+    //   ".one",
+    //   0.7,
+    //   {
+    //     opacity: 0,
+    //     y: 10
+    //   },
+    //   "+=2.5"
+    // )
+    // .to(
+    //   ".two",
+    //   0.7,
+    //   {
+    //     opacity: 0,
+    //     y: 10
+    //   },
+    //   "-=1"
+    // )
+    // .from(".three", 0.7, {
+    //   opacity: 0,
+    //   y: 10
+    //   // scale: 0.7
+    // })
+    // .to(
+    //   ".three",
+    //   0.7,
+    //   {
+    //     opacity: 0,
+    //     y: 10
+    //   },
+    //   "+=2"
+    // )
+    // .from(".four", 0.7, {
+    //   scale: 0.2,
+    //   opacity: 0
+    // })
+    // .from(".fake-btn", 0.3, {
+    //   scale: 0.2,
+    //   opacity: 0
+    // })
+    // .staggerTo(
+    //   ".hbd-chatbox span",
+    //   0.5,
+    //   {
+    //     visibility: "visible"
+    //   },
+    //   0.05
+    // )
+    // .to(".fake-btn", 0.1, {
+    //   backgroundColor: "rgb(127, 206, 248)"
+    // })
+    // .to(
+    //   ".four",
+    //   0.5,
+    //   {
+    //     scale: 0.2,
+    //     opacity: 0,
+    //     y: -150
+    //   },
+    //   "+=0.7"
+    // )
+    // .from(".idea-1", 0.7, ideaTextTrans)
+    // .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    // .from(".idea-2", 0.7, ideaTextTrans)
+    // .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    // .from(".idea-3", 0.7, ideaTextTrans)
+    // .to(".idea-3 strong", 0.5, {
+    //   scale: 1.2,
+    //   x: 10,
+    //   backgroundColor: "rgb(21, 161, 237)",
+    //   color: "#fff"
+    // })
+    // .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    // .from(".idea-4", 0.7, ideaTextTrans)
+    // .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    // .from(
+    //   ".idea-5",
+    //   0.7,
+    //   {
+    //     rotationX: 15,
+    //     rotationZ: -10,
+    //     skewY: "-5deg",
+    //     y: 50,
+    //     z: 10,
+    //     opacity: 0
+    //   },
+    //   "+=0.5"
+    // )
+    // .to(
+    //   ".idea-5 .smiley",
+    //   0.7,
+    //   {
+    //     rotation: 90,
+    //     x: 8
+    //   },
+    //   "+=0.4"
+    // )
+    // .to(
+    //   ".idea-5",
+    //   0.7,
+    //   {
+    //     scale: 0.2,
+    //     opacity: 0
+    //   },
+    //   "+=2"
+    // )
+    // .staggerFrom(
+    //   ".idea-6 span",
+    //   0.8,
+    //   {
+    //     scale: 3,
+    //     opacity: 0,
+    //     rotation: 15,
+    //     ease: Expo.easeOut
+    //   },
+    //   0.2
+    // )
+    // .staggerTo(
+    //   ".idea-6 span",
+    //   0.8,
+    //   {
+    //     scale: 3,
+    //     opacity: 0,
+    //     rotation: -15,
+    //     ease: Expo.easeOut
+    //   },
+    //   0.2,
+    //   "+=1"
+    // )
+    // .staggerFromTo(
+    //   ".baloons img",
+    //   2.5,
+    //   {
+    //     opacity: 0.9,
+    //     y: 1400
+    //   },
+    //   {
+    //     opacity: 1,
+    //     y: -1000
+    //   },
+    //   0.2
+    // )
     .from(
       ".lydia-dp",
       0.5,
@@ -255,34 +255,34 @@ const animationTimeline = () => {
       0.1,
       "party"
     )
-    .from(
-      ".wish h5",
-      0.5,
-      {
-        opacity: 0,
-        y: 10,
-        skewX: "-15deg"
-      },
-      "party"
-    )
-    .staggerTo(
-      ".eight svg",
-      1.5,
-      {
-        visibility: "visible",
-        opacity: 0,
-        scale: 80,
-        repeat: 3,
-        repeatDelay: 1.4
-      },
-      0.3
-    )
-    .to(".six", 0.5, {
-      opacity: 0,
-      y: 30,
-      zIndex: "-1"
-    })
-    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    // .from(
+    //   ".wish h5",
+    //   0.5,
+    //   {
+    //     opacity: 0,
+    //     y: 10,
+    //     skewX: "-15deg"
+    //   },
+    //   "party"
+    // )
+    // .staggerTo(
+    //   ".eight svg",
+    //   1.5,
+    //   {
+    //     visibility: "visible",
+    //     opacity: 0,
+    //     scale: 80,
+    //     repeat: 3,
+    //     repeatDelay: 1.4
+    //   },
+    //   0.3
+    // )
+    // .to(".six", 0.5, {
+    //   opacity: 0,
+    //   y: 30,
+    //   zIndex: "-1"
+    // })
+    // .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
       ".last-smile",
       0.5,
